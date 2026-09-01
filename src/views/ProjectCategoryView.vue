59b470d2043350be496eba3d560435e076a5dc7b
panweippc <2531519370@qq.com>
2026-09-01 11:20:59 +0800

    fix(OA): 导入 computed，修复产品分类页因未定义 computed 导致的空白页

--- src/views/ProjectCategoryView.vue
+++ src/views/ProjectCategoryView.vue
@@ -202,7 +202,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { Edit, Delete, Link, Plus } from '@element-plus/icons-vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
